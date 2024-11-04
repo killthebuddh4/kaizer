070eaeb8a7fa72b48ee99b88c8727c251c1a6781
Achilles Schmelzer <achilles@ktb.pub>
2024-11-04 09:56:12 -0800

    minimal websocket audio stream

--- go.mod
+++ go.mod
@@ -3,3 +3,12 @@ module github.com/killthebuddh4/kaizer
 go 1.18
 
 require github.com/go-chi/chi/v5 v5.1.0
+
+require (
+	github.com/beevik/etree v1.1.0 // indirect
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/twilio/twilio-go v1.23.5 // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,67 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/chi"
+	"github.com/gorilla/websocket"
 )
 
+// Define an upgrader to upgrade HTTP connections to WebSocket connections.
+var upgrader = websocket.Upgrader{
+	// Allow all origins for simplicity. You may want to restrict this in production.
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+	router := chi.NewRouter()
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Goodbye, World!"))
 	})
-	http.ListenAndServe(":8080", r)
+
+	router.Post("/answer", func(w http.ResponseWriter, r *http.Request) {
+		stream := `
+<Response>
+
+    <Connect>
+        <Stream name="Example Audio Stream" url="wss://12eb-98-176-235-185.ngrok-free.app/stream" />
+    </Connect>
+
+    <Say>The stream has started.</Say>
+
+</Response>`
+
+		w.Header().Set("Content-Type", "application/xml")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(stream))
+	})
+
+	router.Get("/stream", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("/stream was called")
+		// Upgrade the HTTP request to a WebSocket connection.
+		ws, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println("Upgrade error:", err)
+			return
+		}
+		defer ws.Close() // Ensure the connection is closed when the function exits
+
+		log.Println("Client connected")
+
+		// Read messages in a loop and log them
+		for {
+			_, msg, err := ws.ReadMessage()
+			if err != nil {
+				log.Println("Read error:", err)
+				break
+			}
+			log.Printf("Received a message of length", len(msg))
+		}
+
+		log.Println("Client disconnected")
+	})
+
+	http.ListenAndServe(":8080", router)
 }
