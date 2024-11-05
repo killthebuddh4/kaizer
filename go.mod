5d0bf756ee2aa30705b41249e9bb9adb91b4f072
Achilles Schmelzer <achilles@ktb.pub>
2024-11-05 08:35:03 -0800

    wip, stream to s3

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require github.com/go-chi/chi/v5 v5.1.0
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.32.3 // indirect
 	github.com/beevik/etree v1.1.0 // indirect
 	github.com/go-audio/audio v1.0.0 // indirect
 	github.com/go-audio/riff v1.0.0 // indirect
@@ -12,6 +13,7 @@ require (
 	github.com/go-chi/chi v1.5.5 // indirect
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/twilio/twilio-go v1.23.5 // indirect
 )
--- main.go
+++ main.go
@@ -7,8 +7,9 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/go-audio/audio"
-	"github.com/go-audio/wav"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/go-chi/chi"
 	"github.com/gorilla/websocket"
 )
@@ -54,6 +55,15 @@ type StopMessage struct {
 }
 
 func main() {
+	awsConfig, err := config.LoadDefaultConfig()
+
+	if err != nil {
+		log.Println("Error loading AWS config:", err)
+		return
+	}
+
+	s3Client := s3.NewFromConfig(awsConfig)
+
 	router := chi.NewRouter()
 
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -65,7 +75,7 @@ func main() {
 <Response>
 
     <Connect>
-        <Stream name="Example Audio Stream" url="wss://7cfb-98-176-235-185.ngrok-free.app/stream" />
+        <Stream name="Example Audio Stream" url="wss://0de3-98-176-235-185.ngrok-free.app/stream" />
     </Connect>
 
     <Say>The stream has started.</Say>
@@ -78,15 +88,18 @@ func main() {
 	})
 
 	router.Get("/stream", func(w http.ResponseWriter, r *http.Request) {
-		ofile, err := os.Create("output.wav")
+		// reader, writer := io.Pipe()
+
+		uploader := s3manager.NewUploader(s3Client)
+
+		ofile, err := os.Create("output.raw")
 
 		if err != nil {
 			log.Println("Error creating output file:", err)
 			return
 		}
 
-		enc := wav.NewEncoder(ofile, 8000, 16, 1, 7)
-		defer enc.Close()
+		defer ofile.Close()
 
 		// Upgrade the HTTP request to a WebSocket connection.
 		ws, err := upgrader.Upgrade(w, r, nil)
@@ -97,14 +110,6 @@ func main() {
 		defer ws.Close() // Ensure the connection is closed when the function exits
 
 		log.Println("Client connected")
-
-		audioBuf := &audio.IntBuffer{
-			Format: &audio.Format{
-				SampleRate:  8000,
-				NumChannels: 1,
-			},
-			Data: make([]int, 8000),
-		}
 
 		// Read messages in a loop and log them
 		for {
@@ -126,49 +131,38 @@ func main() {
 			if raw["event"] == "start" {
 				log.Println("Start messaging received, processing...")
 
-				// var msg StartMessage
-				// err := json.Unmarshal(bytes, &StartMessage{})
-
-				// if err != nil {
-				// 	log.Println("Unmarshal error:", err)
-				// 	break
-				// }
-
 				log.Println("Done processing start message")
 			} else if raw["event"] == "media" {
 				log.Println("Media message received, processing...")
 
 				var msg MediaMessage
-				err := json.Unmarshal(bytes, &MediaMessage{})
+				err := json.Unmarshal(bytes, &msg)
 
 				if err != nil {
 					log.Println("Unmarshal error:", err)
 					break
 				}
 
-				bs, err := base64.StdEncoding.DecodeString(msg.Media.Payload)
+				data, err := base64.StdEncoding.DecodeString(msg.Media.Payload)
 
 				if err != nil {
 					log.Println("Base64 decode error:", err)
 					break
 				}
 
-				ints := make([]int, len(bs))
+				written, err := ofile.Write(data)
 
-				for i, b := range bs {
-					ints[i] = int(b)
+				if err != nil {
+					log.Println("Write error:", err)
+					break
 				}
 
-				audioBuf.Data = append(audioBuf.Data, ints...)
+				// phone-number/yyyy-mm-dd-hh-mm-ss-mmm/n.raw
 
+				log.Println("Wrote", written, "bytes to file")
 				log.Println("Done processing media message")
 			} else if raw["event"] == "stop" {
 				log.Println("Stop message received")
-
-				log.Println("Writing audio data to file")
-
-				enc.Write(audioBuf)
-
 				log.Println("Done processing stop message")
 			}
 		}
